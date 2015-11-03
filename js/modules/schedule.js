e53c46b3bff0113000aefec258dbbc71a23d5f42
Sam Mason <sam.jbmason@gmail.com>
2015-11-03 19:11:03 +0000

    Add hash check for ga anchor id

--- js/modules/schedule.js
+++ js/modules/schedule.js
@@ -16,7 +16,7 @@ class Schedule {
 
   checkHashAndTriggerModal(){
     var hash = window.location.hash;
-    if(hash === '#anchor-js' || hash === '#anchor-sketch' || hash === '#anchor-github'){
+    if(hash === '#anchor-js' || hash === '#anchor-sketch' || hash === '#anchor-github' || hash === '#anchor-ga'){
       this.toggleSchedule('workshops');
     }
     if(hash !== '#workshops' && hash !== '#talks' && hash !== ''){
