4019a45a0801f5a2d6b9961a0b4abf478d77a9cd
Sam Mason <sam.jbmason@gmail.com>
2015-11-10 15:21:24 +0000

    Add hash check for makers in js

--- js/modules/schedule.js
+++ js/modules/schedule.js
@@ -16,7 +16,7 @@ class Schedule {
 
   checkHashAndTriggerModal(){
     var hash = window.location.hash;
-    if(hash === '#anchor-js' || hash === '#anchor-sketch' || hash === '#anchor-github' || hash === '#anchor-ga'){
+    if(hash === '#anchor-js' || hash === '#anchor-sketch' || hash === '#anchor-github' || hash === '#anchor-ga' || hash === '#anchor-ma'){
       this.toggleSchedule('workshops');
     }
     if(hash !== '#workshops' && hash !== '#talks' && hash !== ''){
